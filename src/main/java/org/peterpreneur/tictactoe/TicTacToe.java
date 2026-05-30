420e10635517abf07406ae54408a9e9f356c95ea
Peter S <peterpreneur@gmail.com>
2026-05-30 16:29:20 -0500

    one

--- src/main/java/org/peterpreneur/tictactoe/TicTacToe.java
+++ src/main/java/org/peterpreneur/tictactoe/TicTacToe.java
@@ -4,6 +4,8 @@
 
 package org.peterpreneur.tictactoe;
 
+import java.util.Scanner;
+
 /**
  *
  * @author peter
@@ -11,6 +13,34 @@ package org.peterpreneur.tictactoe;
 public class TicTacToe {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Tic Tac Toe!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name the first player:");
+        String firstName = scanner.nextLine();
+        System.out.println("firstName: " + firstName);
+
+        System.out.println("Name the second plahyer:");
+        String secondName = scanner.nextLine();
+        System.out.println("secondName: " + secondName);        
+
+        //initialize the board
+
+        //print the board
+
+        //ask the current player for a move
+
+        //validate the move
+
+        //apply the move
+
+        //check win or draw
+
+        //switch player
+
+        scanner.close();
+
+
     }
 }
