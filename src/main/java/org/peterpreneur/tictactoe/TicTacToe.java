3583ec982bfed811c08863f5393ca78670c961de
Peter S <peterpreneur@gmail.com>
2026-05-30 12:32:03 -0500

    init commit

---
+++ src/main/java/org/peterpreneur/tictactoe/TicTacToe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.peterpreneur.tictactoe;
+
+/**
+ *
+ * @author peter
+ */
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
