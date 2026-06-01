8ccde7ee757b0533e299c194ff3a3d20b2492606
Peter S <peterpreneur@gmail.com>
2026-05-31 23:07:27 -0500

    temp commit

--- src/main/java/org/peterpreneur/tictactoe/TicTacToe.java
+++ src/main/java/org/peterpreneur/tictactoe/TicTacToe.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.peterpreneur.tictactoe;
 
 import java.util.Scanner;
@@ -12,35 +11,101 @@ import java.util.Scanner;
  */
 public class TicTacToe {
 
+    static char currentPlayer = 'X';
+    static Scanner scanner = new Scanner(System.in);
+    static char[][] board = new char[3][3];
+    static char[] t3Board = new char[9];
+
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe!");
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Name the first player:");
-        String firstName = scanner.nextLine();
-        System.out.println("firstName: " + firstName);
-
-        System.out.println("Name the second plahyer:");
-        String secondName = scanner.nextLine();
-        System.out.println("secondName: " + secondName);        
-
+        // System.out.println("Name the first player:");
+        // String firstName = scanner.nextLine();
+        // System.out.println("firstName: " + firstName);
+        // System.out.println("Name the second plahyer:");
+        // String secondName = scanner.nextLine();
+        // System.out.println("secondName: " + secondName);
         //initialize the board
+        initializeBoard(t3Board);
 
         //print the board
+        printBoard(t3Board);
 
-        //ask the current player for a move
+        boolean continueFlag = true;
 
-        //validate the move
+        do {
+            //ask the current player for a move
+            askForMove();
 
-        //apply the move
+            //validate the move
+            isValidMove(t3Board);
+
+            //apply the move
+            printBoard(t3Board);
+
+            // check if any space is still empty 
+            // if no, then stop
+            // if yes, then check if the selected space is still empty
+            // if no, then ask user to select another space
+            // if yes, then check if there's a 3 cross winner
+            // if no, then switch to next player 
+            // if yes, then sout "Player X win!"
+            continueFlag = continueMove(t3Board);
+        } while (continueFlag);
 
         //check win or draw
-
         //switch player
-
         scanner.close();
+    }
 
+    private static void initializeBoard(char[] t3Board) {
+        for (int i = 0; i < t3Board.length; i++) {
+            t3Board[i] = (char) ('1' + i);
+        }
+    }
 
+    private static void printBoard(char[] t3Board) {
+        for (int i = 0; i < t3Board.length; i++) {
+
+            System.out.print(t3Board[i] + " ");
+
+            int modVal = i % 3;
+            if (modVal == 2) {
+                System.out.println();
+            }
+        }
+    }
+
+    public static void askForMove() {
+        System.out.println("Enter your move.");
+
+        boolean repeat = false;
+
+        do {
+            System.out.println("Enter location (1-9): ");
+            int cell = scanner.nextInt();
+
+            if (isValid(cell)) {
+                t3Board[cell - 1] = currentPlayer;
+                break;
+            }
+
+        } while (repeat);
+
+    }
+
+    private static boolean isValid(int cell) {
+        char cellVal = t3Board[cell - 1];
+        System.out.println("cellVal=" + cellVal);
+
+        return !(cellVal == 'X' || cellVal == 'O');
+    }
+
+    private static void isValidMove(char[] t3Board) {
+
+    }
+
+    private static boolean continueMove(char[] t3Board) {
+        return true;
     }
 }
