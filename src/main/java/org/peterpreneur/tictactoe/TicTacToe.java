c40e5fec091ddcc4fe7fad74981e79d4f62b96f7
Peter S <peterpreneur@gmail.com>
2026-06-01 22:08:34 -0500

    finalize

--- src/main/java/org/peterpreneur/tictactoe/TicTacToe.java
+++ src/main/java/org/peterpreneur/tictactoe/TicTacToe.java
@@ -13,29 +13,17 @@ public class TicTacToe {
 
     static char currentPlayer;
     static Scanner scanner = new Scanner(System.in);
-    static char[][] board = new char[3][3];
     static char[] t3Board = new char[9];
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe!");
 
-        // System.out.println("Name the first player:");
-        // String firstName = scanner.nextLine();
-        // System.out.println("firstName: " + firstName);
-        // System.out.println("Name the second plahyer:");
-        // String secondName = scanner.nextLine();
-        // System.out.println("secondName: " + secondName);
-        //initialize the board
         initializeBoard(t3Board);
-
-        //print the board
         printBoard(t3Board);
 
-        boolean continueFlag = true;
+        boolean gameOver = false;
 
-        //do until all cells are filled or somebody one
-        //since there are 9 elements in Array do a for loop
-        for (int i = 0; i < t3Board.length; i++) {
+        for (int i = 0; i < t3Board.length && !gameOver; i++) {
             if (i % 2 == 0) {
                 currentPlayer = 'X';
             } else {
@@ -43,30 +31,25 @@ public class TicTacToe {
             }
 
             askToMove(currentPlayer);
+
+            // Check if current player won
+            if (checkWinner(currentPlayer)) {
+                System.out.println("\nPlayer " + currentPlayer + " WINS!");
+                gameOver = true;
+                break;
+            }
+
+            // Check for tie (board is full)
+            if (isBoardFull()) {
+                System.out.println("\nIt's a TIE!");
+                gameOver = true;
+            }
         }
 
-        do {
-            //ask the current player for a move
-            // askForMove();
+        if (!gameOver) {
+            System.out.println("\nGame ended unexpectedly");
+        }
 
-            //validate the move
-            // isValidMove(t3Board);
-            //apply the move
-            printBoard(t3Board);
-
-            // check if any space is still empty 
-            // if no, then stop
-            // if yes, then check if the selected space is still empty
-            // if no, then ask user to select another space
-            // if yes, then check if there's a 3 cross winner
-            // if no, then switch to next player 
-            // if yes, then sout "Player X win!"
-            // continueFlag = continueMove(t3Board);
-        } while (true);
-
-        //check win or draw
-        //switch player
-        // scanner.close();
     }
 
     private static void initializeBoard(char[] t3Board) {
@@ -88,7 +71,7 @@ public class TicTacToe {
     }
 
     public static void askToMove(char currentPlayer) {
-        int input = 0;
+        boolean flag = false;
 
         do {
             System.out.println("Player " + currentPlayer + " ,enter location (1-9): ");
@@ -96,19 +79,56 @@ public class TicTacToe {
 
             if (isValid(cell)) {
                 t3Board[cell - 1] = currentPlayer;
-                input = 1;
+                flag = true;
             } else {
                 System.out.println("Invalid!");
             }
             printBoard(t3Board);
-        } while (input == 0);
+
+        } while (!flag);
     }
 
     private static boolean isValid(int cell) {
-        char cellVal = t3Board[cell - 1];
-        System.out.println("cellVal=" + cellVal);
+        // Check if cell is in valid range (1-9)
+        if (cell < 1 || cell > 9) {
+            return false;
+        }
 
+        char cellVal = t3Board[cell - 1];
+
+        // Check if cell not already X nor O
         return !(cellVal == 'X' || cellVal == 'O');
     }
 
+    private static boolean checkWinner(char player) {
+        // All 8 winning combination
+        int[][] winningCombos = {
+            {1, 2, 3}, {4, 5, 6}, {7, 8, 9}, // rows
+            {1, 4, 7}, {2, 5, 8}, {3, 6, 9}, // columns
+            {1, 5, 9}, {3, 5, 7} // diagonals
+        };
+
+        // Check each winning combination
+        for (int[] combo : winningCombos) {
+            char a = t3Board[combo[0] - 1];
+            char b = t3Board[combo[1] - 1];
+            char c = t3Board[combo[2] - 1];
+
+            // If all 3 position match the current player
+            if (a == player && b == player && c == player) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Check if board is full (tie game)
+    private static boolean isBoardFull() {
+        for (int i = 0; i < t3Board.length; i++) {
+            if (t3Board[i] >= '1' && t3Board[i] <= '9') {
+                return false;
+            }
+        }
+        return true;
+    }
 }
